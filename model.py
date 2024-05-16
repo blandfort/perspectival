2b3f7937df9c8e38d46d7d4e40b5eeca50cd034b
Philipp Blandfort <philipp.blandfort@rtl-extern.de>
2024-05-16 23:40:35 +0200

    add model class which only loads model on computation

--- model.py
+++ model.py
@@ -7,7 +7,64 @@ from interfaces import Model
 from dataset import Item
 
 
-class LLMModel(Model):
+class LazyTransformer(Model):
+    """Class to use transformer models while only loading them when needed"""
+
+    def __init__(self, model_name):
+        self.name = model_name
+
+    def compute_option_log_likelihoods(
+            self,
+            items: List[Item],
+            add_whitespace: bool=True,
+            **kwargs,
+        ) -> List[float]:
+        # Set up
+        model = AutoModelForCausalLM.from_pretrained(self.name)
+        tokenizer = AutoTokenizer.from_pretrained(self.name)
+        if tokenizer.pad_token is None:
+            tokenizer.pad_token = tokenizer.eos_token
+        model.eval()  # Set the model to evaluation mode
+
+        def compute_logits(encoded_texts):
+            with torch.no_grad():
+                outputs = model(**encoded_texts, **kwargs)
+                return outputs.logits
+
+        # Initialize return list
+        log_likelihoods = []
+
+        for item in items:
+            input_texts = [item.prompt + (" " if add_whitespace else "") + option for option in item.options]
+
+            # Tokenize the combined texts
+            encoded_inputs = tokenizer(input_texts, padding=True, truncation=True, return_tensors='pt')
+
+            # Get logits from the model
+            logits = compute_logits(encoded_inputs)
+
+            # Ignore the log probs at the beginning
+            prompt_length = len(tokenizer(item.prompt, add_special_tokens=False)['input_ids']) #- 1
+
+            # Calculate log probabilities from the logits for each token
+            log_probs = F.log_softmax(logits, dim=-1)[:, prompt_length - 1:-1]
+            # Need to offset by one since last position contains prediction for current token
+
+            input_ids = encoded_inputs['input_ids'][:, prompt_length:]
+            attention_mask = encoded_inputs['attention_mask'][:, prompt_length:]
+
+            # Get log probabilities of actual tokens
+            token_log_probs = log_probs.gather(2, input_ids.unsqueeze(-1)).squeeze(-1)
+
+            # Set irrelevant entries at the end (from padding) to zero
+            masked_log_probs = token_log_probs * attention_mask
+
+            item_log_likelihoods = torch.sum(masked_log_probs, dim=1).numpy()
+            log_likelihoods.append(item_log_likelihoods)
+        return log_likelihoods
+
+
+class Transformer(Model):
     def __init__(self, model_name):
         """
         Initialize the Model with a transformer model and tokenizer from Hugging Face.
