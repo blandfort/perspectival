5e5cce8666a5d13ddd051af9d386a46f981ff20f
Philipp Blandfort <philipp.blandfort@rtl-extern.de>
2024-06-13 14:27:35 +0200

    removing OpenELM from tests
    OpenELM models use the Llama tokenizer, which is part of a gated
    repo. This caused issues in the CICD pipeline.

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,11 +7,9 @@ from perspectival.model import Transformer
 @pytest.fixture(
     params=[
         ("gpt2", False),
-        ("apple/OpenELM-270M", False),
     ]
 )
 def transformer_model(request):
-    # TODO problem in cicd pipeline will be that this model needs to be downloaded...
     model_name, lazy_loading = request.param
     return Transformer(
         model_name, lazy_loading=lazy_loading, model_kwargs={"trust_remote_code": True}
